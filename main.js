f02a27f92653ed1e1620d80246eab7a8c4c92c15
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2023-09-04 22:28:12 +0200

    Small fixes and filled great parts of the sites.

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-// set the time in format hh:mm:ss in the span with id="clock" every second 
+// Desc: Main javascript file for the website
+
+//local ip addresse, kjekt for å koble opp til server senere
+var ip_pos = document.getElementById('ip');
+ip_pos.innerHTML = window.location.hostname;
+
+//get navbar from navbar.html and place into div with navbar class in every page where this div is
+$(function(){
+    $("#navbar").load("nav.html");
+  });
+
+//Klokken
 var clock_pos = document.getElementById('clock');
 var timer = setInterval(function() {
     clock_pos.innerHTML = new Date().toLocaleTimeString();
 }, 1000);
-var ip_pos = document.getElementById('ip');
-ip_pos.innerHTML = window.location.hostname;
-
-//get navbar from navbar.html
-var nav_pos = document.getElementsByClassName('navbar');
-nav_pos.innerHTML = $("#navbar").load("navbar.html");
