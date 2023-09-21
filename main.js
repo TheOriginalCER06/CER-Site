6845d09680c5eae8f070cad8a97f7df0122672f4
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2023-09-21 23:32:20 +0200

    added to another peice of code

--- main.js
+++ main.js
@@ -2,13 +2,14 @@
 
 //local ip addresse, kjekt for å koble opp til server senere
 var ip_pos = document.getElementById('ip');
-//check if ip_pos is null
 if (ip_pos != null) {
     ip_pos.innerHTML = window.location.hostname;
 }
 
 //Klokken
 var clock_pos = document.getElementById('clock');
-var timer = setInterval(function() {
-    clock_pos.innerHTML = new Date().toLocaleTimeString();
-}, 1000);
+if (clock_pos != null) {
+    var timer = setInterval(function() {
+        clock_pos.innerHTML = new Date().toLocaleTimeString();
+    }, 1000);
+}
