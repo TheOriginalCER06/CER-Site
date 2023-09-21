a46016a156d2bec8b14c8879bf7a2672cd169162
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2023-09-21 23:30:28 +0200

    check if item is used

--- main.js
+++ main.js
@@ -2,7 +2,10 @@
 
 //local ip addresse, kjekt for å koble opp til server senere
 var ip_pos = document.getElementById('ip');
-ip_pos.innerHTML = window.location.hostname;
+//check if ip_pos is null
+if (ip_pos != null) {
+    ip_pos.innerHTML = window.location.hostname;
+}
 
 //Klokken
 var clock_pos = document.getElementById('clock');
