f142e09c130372ad2c721210125c1be822323282
TheOriginalCER06 <69149072+TheOriginalCER06@users.noreply.github.com>
2023-10-25 09:23:32 +0200

    small changes

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // Desc: Main javascript file for the website
 
 //local ip addresse, kjekt for å koble opp til server senere
-var ip_pos = document.getElementById('ip');
+let ip_pos = document.getElementById('ip');
 if (ip_pos != null) {
     ip_pos.innerHTML = window.location.hostname;
 }
 
 //Klokken
-var clock_pos = document.getElementById('clock');
+let clock_pos = document.getElementById('clock');
 if (clock_pos != null) {
     var timer = setInterval(function() {
         clock_pos.innerHTML = new Date().toLocaleTimeString();
